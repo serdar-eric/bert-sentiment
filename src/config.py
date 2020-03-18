4dd87736fa83bb42156cfd5f2e1f537a17fe0fa5
Serdar Eriç <serdar.eric@nesine.com>
2020-03-18 15:10:22 +0300

    Changed config file

--- src/config.py
+++ src/config.py
@@ -2,13 +2,13 @@ import transformers
 
 
 MAX_LEN = 512
-TRAIN_BATCH_SIZE = 8
-VALID_BATCH_SIZE = 4
+TRAIN_BATCH_SIZE = 2
+VALID_BATCH_SIZE = 2
 EPOCHS = 10
-BERT_PATH = "../input/bert_base_uncased/"
+BERT_PATH = "bert-base-uncased" #"../input/bert_base_uncased/"
 MODEL_PATH = "model.bin"
 TRAINING_FILE = "../input/imdb.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(
-    BERT_PATH,
+    "bert-base-uncased",
     do_lower_case=True
 )
